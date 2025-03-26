ecaef272f4096b786ddc4b1c4e6c2bd901511522
leotfr <leotafforeau@gmail.com>
2025-03-26 11:47:09 +0100

    task-0.c

--- task-0.c
+++ task-0.c
@@ -1,52 +1,51 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
-#include <main.h>
+#include "main.h"
+
 /**
-* print: a function that produces output according to a format
-* Returns: the number of characters printed,
-* excluding the null byte used to end output to strings
-*
-* Description: write output to stdout, the standard output stream
+* _printf - Prints anything based on the format provided.
+* @format: A list of arguments passed to the function.
+* Return: The total number of characters printed.
 */
-int _printf(const char *format, ...);
-va_list args;
-int count = 0;
-va_start(args, format);
-for (int i = 0; fotmat[i] != '\0'; i++) {
-if (format[i] == '%' && format[i + 1] != '\0') {
-i++;
-if (format[i] == 'c') {
-char c = va_arg(args, int);
-putchar(c);
-count++;
-}
-else if (format[i] == 's') {
-char *str = va_arg(args, char *)
-while (*str) {
-putchar(*str++);
-count++;
-}
-}
-else if (format[i] == '%') {
-putchar('%');
-count++;
-}
-else {
-putchar('%');
-putchar(format[i]);
-count += 2;
-}
-}
-else {
-putchar(format[i]);
-count++
-}
-}
-va_end(args);
-return count;
-}
-int main();
+int _printf(const char *format, ...)
 {
-_printf("Hello %s! Your grade is %c%%\n", "Alice", 'A');
-return 0;
-}
+    va_list args; 
+    unsigned int format_count = 0, lenght = 0, specifier_count;
+    int (*print_functions[])(va_list) = {
+        print_char, print_string, print_decimal, print_integer, print_percent};
+    char format_specifiers[] = {'c', 's', 'd', 'i', '%'};
+
+    if (format == NULL)
+        return (-1);
+    va_start (args, format);
+    while (format [format_count])
+    {
+        if (format[format_count] == '%')
+        {
+            format_count++;
+            if (format[format_count] == '\0')
+            {
+                return (-1); }
+            for (specifier_count = 0; specifier_count < 5; specifier_count++)
+            {
+                if (format[format_count] == format_specifiers[specifier_count])
+                {
+                    length += print_functions[specifier_count](args);
+                    break; }
+                }
+                if (specifier_count == 5)
+                {
+                    _putchar('%');
+                        _putchar(format[format_count]);
+                        length += 2;
+                    }    
+        }
+        else
+        {
+            length += _putchar(format[format_count]);
+            }
+        format_count++;
+    }
+    va_end(args);
+    return (lenght);
+} 
